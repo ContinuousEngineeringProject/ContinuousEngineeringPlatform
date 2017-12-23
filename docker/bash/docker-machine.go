c1580dc058e2a17cf160ae4d17d4bae06c9b0c62
cvjt <chris@hillsheepstudios.com>
2017-12-23 17:59:50 +0000

    Create multiple virtual machines
    [Finished #152753276] Create VirtualBox VM
    
    Signed-off-by: cvjt <chris@hillsheepstudios.com>

--- docker/bash/docker-machine.go
+++ docker/bash/docker-machine.go
@@ -3,58 +3,36 @@ package bash
 import (
 	"fmt"
 	"os"
-	"bufio"
 	"os/exec"
+	"strconv"
 )
 
-const dCmdName = "docker-machine"
+const cmdName = "docker-machine"
 
-type ArgsVirtualbox struct {
+type ArgsCreateVM struct {
 	NAME 	string
 	DRIVER 	string
 	MEMORY 	string
 	CPU 	string
+	COUNT 	int
 }
 
-func CreateVM(options ArgsVirtualbox) (*exec.Cmd) {
+func CreateVM(options ArgsCreateVM) {
 	dCmd := "create"
-	switch options.DRIVER {
-	case "virtualbox":
-		dCmdAgrs := []string{dCmd, "-d", options.DRIVER, "--virtualbox-memory", options.MEMORY, "--virtualbox-cpu-count", options.CPU, options.NAME}
-		fmt.Fprintln(os.Stderr, "Creating virtual machine " + options.NAME)
-		return exec.Command(dCmdName, dCmdAgrs...)
-	default:
-		fmt.Fprintln(os.Stderr, "Error unknown driver", options.DRIVER)
-		os.Exit(1)
-	}
 
-	fmt.Fprintln(os.Stderr, "Error creating VM", options.NAME)
-	return exec.Command(dCmdName)
-}
+	for vm := 1; vm <= options.COUNT; vm++ {
+		fmt.Fprintln(os.Stderr, "Creating virtual machine " + options.NAME + strconv.Itoa(vm) + "...")
 
-func RunCmd(cmd *exec.Cmd)  {
-	cmdReader, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-		os.Exit(1)
-	}
-
-	scanner := bufio.NewScanner(cmdReader)
-	go func() {
-		for scanner.Scan() {
-			fmt.Printf("docker build out | %s\n", scanner.Text())
+		switch options.DRIVER {
+		case "virtualbox":
+			dCmdAgrs := []string{dCmd, "-d", options.DRIVER, "--virtualbox-memory", options.MEMORY, "--virtualbox-cpu-count", options.CPU, options.NAME + strconv.Itoa(vm)}
+			runBashCmd(exec.Command(cmdName, dCmdAgrs...))
+			fmt.Fprintln(os.Stderr, "Virtual machine " + options.NAME + strconv.Itoa(vm) + " created")
+		default:
+			fmt.Fprintln(os.Stderr, "Error unknown driver", options.DRIVER)
+			os.Exit(1)
 		}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
-		os.Exit(1)
 	}
-
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-		os.Exit(1)
-	}
+	return
 }
+
